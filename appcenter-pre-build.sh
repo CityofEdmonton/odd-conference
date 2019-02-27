44965a6f9adc5c2560c0e820acff0121ed234075
Dan Chenier <daniel.chenier@edmonton.ca>
2019-02-27 10:25:17 -0700

    Add FacebookSDK to build pipeline

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,5 @@
+#download facebok sdk
+curl https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip --output fsdk.zip
+
+#unzip
+unzip fsdk.zip -d ~/Documents/FacebookSDK
